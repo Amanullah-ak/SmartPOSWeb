76402a2a00ccecd4d32cf07a55f5860c606e539b
Aman Ullah <amanullah43085@gmail.com>
2024-07-23 02:17:24 +0500

    Add project files.

---
+++ SmartPOSWebApp/SmartPOSContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartPOSWebApp.Models;
+namespace SmartPOSWebApp
+{
+    public class SmartPOSContext : DbContext 
+    {
+        public SmartPOSContext(DbContextOptions<SmartPOSContext> options) 
+            : base(options) 
+        {                
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
